a3ea3ab7471643e681f7b33e83efe57cb35228c8
7ap <109433659+7ap@users.noreply.github.com>
2022-10-24 11:27:35 -0500

    feat: add winit scaffolding

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,27 @@
+use winit::event::*;
+use winit::event_loop::EventLoop;
+use winit::window::WindowBuilder;
+
 pub async fn run() {
     log::info!("Starting...");
 
-    /* ... */
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
 
-    log::info!("Exiting...");
+    event_loop.run(move |event, _, control_flow| {
+        control_flow.set_poll();
+
+        match event {
+            Event::WindowEvent {
+                ref event,
+                window_id,
+            } if window_id == window.id() => match event {
+                WindowEvent::CloseRequested => control_flow.set_exit(),
+                _ => {}
+            },
+            Event::MainEventsCleared => window.request_redraw(),
+            Event::RedrawRequested(window_id) if window_id == window.id() => {}
+            _ => (),
+        }
+    });
 }
