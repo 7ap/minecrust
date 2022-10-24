75e31469a2a107341d09518fa31b33f1fa71d69f
7ap <109433659+7ap@users.noreply.github.com>
2022-10-24 11:30:56 -0500

    feat: stub state system

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,21 @@ use winit::event::*;
 use winit::event_loop::EventLoop;
 use winit::window::WindowBuilder;
 
+struct State {}
+
+impl State {
+    async fn new() -> Self {
+        Self {}
+    }
+}
+
 pub async fn run() {
     log::info!("Starting...");
 
     let event_loop = EventLoop::new();
     let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+    let mut state = State::new().await;
 
     event_loop.run(move |event, _, control_flow| {
         control_flow.set_poll();
